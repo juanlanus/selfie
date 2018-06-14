7a8c7786cfdcc499b294943073d5a049656f9a48
Juan Lanus <juan.lanus@gmail.com>
2018-06-14 17:50:25 -0300

    added folders acua, infoNews, medis, telephony to /docs

---
+++ docs/infoNews/res/doc.js
@@ -0,0 +1,46 @@
+  $(function() {
+
+    // create and display TOC
+    $('#tocContainer').tocBuilder({ 
+      type: 'headings', 
+      backLinkText: 'TOC', 
+      startLevel: 2, 
+      endLevel: 6
+    });
+
+    // prevent TOC links from changing the location hash
+    $('#tocContainer a, .tocBackLink').on(
+      'click',
+      function(e) {
+        e.preventDefault();
+        // console.log('TOC link clicked ' + e.currentTarget.hash);
+        $('body, html').animate({
+            'scrollTop': $(e.currentTarget.hash).offset().top - 33
+        }, 500);
+      }
+    )
+
+    // on scroll, make the top fixed heading dissapear
+    $( window ).scroll(
+      function(){
+
+        var position = $(this).scrollTop();
+        var opacity;
+        if( position > 200 ) {
+          opacity = 0;
+        } else if( position < 10 ) {
+          opacity = 1;
+        } else {
+          opacity = 1 / ( position * position / 8000 );
+        };
+        $( '.fixedHeader' ).css( 'opacity', opacity );
+        if( opacity === 0 ){ // move it out of the way
+          $( '.fixedHeader' ).css( 'margin-top', -1000 );
+        } else {
+          $( '.fixedHeader' ).css( 'margin-top', 0 );
+        };
+      }
+    );
+
+
+});
